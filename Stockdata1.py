2b36293d044edf275b2e4dacb84275ba2ab3d647
mikebostanci <mikebostanci@yahoo.com>
2024-06-02 13:42:53 -0400

    Update Stockdata1.py 2.
    Added 2.

--- Stockdata1.py
+++ Stockdata1.py
@@ -1,1 +1,3 @@
-
+https://colab.research.google.com/drive/1lwfgadL0z3Ct5EAjktNKQfVRzvUK2oFs#scrollTo=wXwtp_nDVFr-
+https://colab.research.google.com/drive/1SdehM8ZefyFMjpyXErgmBjfpI8KM0v2P#scrollTo=_3nWyCGDbqJy
+https://colab.research.google.com/drive/12yw0sIyZPUfiUfgxIg9mEe6Jmmu9PWkz#scrollTo=jP-pZ3eIOhSA&uniqifier=5
